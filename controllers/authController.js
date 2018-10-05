8222adcd02d08121e4549811bdaba2eb8ca8cdd8
Ramon Victor <ramonvictorn@gmail.com>
2018-10-05 16:34:52 -0300

    token 0.1

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,15 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs')
 const User = require('../models/user');
-
-
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth')
 const router = express.Router();
 
 
 router.post('/register', async(req,res)=>{
     const {email} = req.body;
     try {
-        
+        console.log('try register', {email})
         if(await User.findOne({email})){
             return res.status(400).send({error:'usuario já existe'})
         }
@@ -18,9 +18,28 @@ router.post('/register', async(req,res)=>{
 
         return res.send({user});
     } catch (err){
+        console.log('erro', err)
         return res.status(400).send({error:'registration failed'});
     }
 });
 
+router.post('/authenticate', async (req,res)=> {
+    const {email,password} = req.body;
+   
+    const user = await User.findOne({email}).select('+password');
+    console.log('/authenticate ', {email,password}, 'user', user)
+    if(!user){
+        return res.status(400).send({error:"User not found"});
+    }
+    if(!await bcrypt.compare(password,user.password)){
+        return res.status(400).send({error:"Invalid Password"});
+    }
+    user.password = undefined;
+    
+    const token = jwt.sign({id:user.id},authConfig.secret, {
+        expiresIn:86400,
+    })
+    res.send({user,token});
+})
 
 module.exports = app => app.use('/auth', router)
--- database/index.js
+++ database/index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 
 mongoose.connect('mongodb://localhost:27017/',{ useNewUrlParser: true });
+// mongoose.connect('mongodb://ramon:Lantec2018@ds125912.mlab.com:25912/agendador-beta',{ useNewUrlParser: true });
+
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
 
